10a2c46364c4063772a24ca796c088523e9c3ef9
shanto096 <shanto096@gmail.com>
2022-12-16 20:30:41 +0600

    e

---
+++ script.js
@@ -0,0 +1,46 @@
+const GeneratorBtn = document.getElementById("generator-btn")
+const inputPin = document.getElementById("input-pin")
+
+
+function generator() {
+  let pin = Math.round(Math.random()*10000)
+  let newPin =pin +"";
+  if (newPin.length === 4) {
+      return pin;  
+  }else{
+      return generator();
+  }
+}
+
+GeneratorBtn.addEventListener('click', function () {
+  const GeneratorPin = document.getElementById("pin-generator")
+  GeneratorPin.innerText = generator()
+})
+
+document.getElementById("all-input").addEventListener('click',function ( event) {
+  let show = event.target.value;
+  let showpin =inputPin.value +show;
+  inputPin.value = showpin;
+   })
+
+  document.getElementById("submitBtn").addEventListener('click',function () {
+  const GeneratorPin = document.getElementById("pin-generator")
+      const x = Number(GeneratorPin.innerText)
+      const y = Number(inputPin.value)
+      if (x === y) {
+          document.getElementById('checkbox1').style.visibility = "visible";
+          // const checkbox1 = document.getElementById('checkbox1')
+          // checkbox1.style.visibility ="visible"
+          // console.log(checkbox1);
+      }else{
+          const checkbox2 = document.getElementById('checkbox2')
+          checkbox2.style.visibility ="visible"
+      }
+  })
+
+  function clearAll() {
+    inputPin.value ="";
+ }
+ document.getElementById("clear").addEventListener('click',function () {
+     clearAll();
+ })
